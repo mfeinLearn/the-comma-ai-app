a0100c246cdfebaa6a70b64c920ce29853dd7b01
Malcome Fein <mfein90@gmail.com>
2024-12-20 13:38:08 -0500

    add button and iframe

--- the-comma-ai-app/src/App.js
+++ the-comma-ai-app/src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+          <>
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/NmBfgOanCyk?si=WkkphgQnRWYKtTxK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+          </>
+          
+            <button style={{
+                display: 'block',
+                margin: 'auto',
+                marginTop: '200px',
+                backgroundColor: '#04AA6D',
+                color: 'white',
+                fontFamily: 'Source Sans Pro, sans-serif',
+                fontSize: '18px',
+                padding: '6px 25px',
+                marginTop: '4px',
+                borderRadius: '5px',
+                wordSpacing: '10px'
+            }}>
+                Run
+            </button>
     </div>
   );
 }
